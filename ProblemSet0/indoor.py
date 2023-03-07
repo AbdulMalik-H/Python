8256420b0a5e0115430ea170e53318f0800827e9
unknown <abdulmalik.alhallaq@outlook.com>
2023-03-08 02:30:31 +0300

    Solution of Indoor Voice problem

---
+++ ProblemSet0/indoor.py
@@ -0,0 +1,5 @@
+# prompt user to enter a message
+# get the input and turn it in lowercase
+user_message = input("Please! Enter your message: ").lower()
+# output the user message in lowercase 
+print(user_message)
