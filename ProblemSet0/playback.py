4024db01c52a9a90a65dcf0702a60592c8cfb11a
unknown <abdulmalik.alhallaq@outlook.com>
2023-03-08 03:00:38 +0300

    Solution of Playback Speed Voice problem

---
+++ ProblemSet0/playback.py
@@ -0,0 +1,5 @@
+# prombt user to enter a message
+user_message = input("Please! Enter your message: ").replace(" ", "...")
+
+# output user message by replacing ever space with three dots
+print(user_message)
