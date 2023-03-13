dcb25b6de3a3e732f2e383d85f6f854be2f4d4f9
unknown <abdulmalik.alhallaq@outlook.com>
2023-03-14 02:52:02 +0300

    add return to the fuctions

--- PSet0/tip.py
+++ PSet0/tip.py
@@ -1,20 +1,19 @@
-# main function coded by CS50
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
 
-# dollars_to_float, which should accept a str as input (formatted as $##.##, wherein each # is a decimal digit)
-# remove the leading $, and return the amount as a float
+
 def dollars_to_float(d):
-    d = float(d.replace('$', ''))
+    d = d.replace('$','')
+    d = float(d)
     return d
 
-# percent_to_float, which should accept a str as input (formatted as ##%, wherein each # is a decimal digit)
-# remove the trailing %, and return the percentage as a float
 def percent_to_float(p):
-    p = float(p.replace('%', ''))/100
+    p = p.replace('%','')
+    p = float(p)/100
     return p
+
 
 main()
