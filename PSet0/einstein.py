1a083a3572c6fd92d9ad0d8e1e6749c4a157c687
unknown <abdulmalik.alhallaq@outlook.com>
2023-03-14 02:51:10 +0300

    fix the equation

--- PSet0/einstein.py
+++ PSet0/einstein.py
@@ -2,7 +2,8 @@
 mass = int(input("To calculate the Enrgy using Einstein equation, Please! Enter a mass (in kilogram): "))
 
 # Einstein Equation
-enrgy = mass * 300000000
+c2 = pow(300000000,2)
+enrgy = mass * c2
 
 print("The Enrgy by Einstein equation is: ", end='')
 print("E=", enrgy, "Joules", sep=' ')
