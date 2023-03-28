928444364c8a8a59b2e38398ebff35a7a7a1ac07
AbdulMalik al-Hallaq <34608585+AbdulMalik-H@users.noreply.github.com>
2023-03-28 10:19:54 +0300

    Delete PSet0 directory

--- PSet0/einstein.py
+++
@@ -1,9 +0,0 @@
-# prompt user to enter a mass mass as an integer (in kilograms)
-mass = int(input("To calculate the Enrgy using Einstein equation, Please! Enter a mass (in kilogram): "))
-
-# Einstein Equation
-c2 = pow(300000000,2)
-enrgy = mass * c2
-
-print("The Enrgy by Einstein equation is: ", end='')
-print("E=", enrgy, "Joules", sep=' ')
