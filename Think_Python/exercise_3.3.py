4f3234dbe806bef3c85d67b2d2a1f43f4308ffd7
AbdulMalik <abdulmalik.alhallaq@outlook.com>
2023-04-09 00:05:11 +0300

    add new exercise

---
+++ Think_Python/exercise_3.3.py
@@ -0,0 +1,16 @@
+# Note: This exercise should be done using only the statements and other features we
+# have learned so far.
+
+# 1. Write a function that draws a grid like the following
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+# |         |         |
+# |         |         |
+# |         |         |
+# |         |         |
+# + - - - - + - - - - +
+
